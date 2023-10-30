264e6a7035d7f7a6bbc92109365eb8312316d551
yoda-gell <hvuskv6@gmail.com>
2023-10-30 17:10:13 +0200

    finish white yellow filter

--- functions/yellow_white_filter.py
+++ functions/yellow_white_filter.py
@@ -1,2 +1,11 @@
-def yellow_white_filter():
-    pass
+import numpy as np
+import cv2 as cv
+
+def white_yellow_filter(frame):
+        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+        lower_white = np.array([0, 0, 0], np.uint8)
+        upper_yellow = np.array([255, 100, 240], np.uint8)
+        
+        yellow_white_frame = cv.inRange(hsv, lower_white, upper_yellow)
+        return yellow_white_frame
+      
