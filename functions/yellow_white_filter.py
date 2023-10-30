fe34cade8a8720993e35aa1481880326eade0004
yoda-gell <hvuskv6@gmail.com>
2023-10-30 19:04:12 +0200

    combine roi and white yellow filter

--- functions/yellow_white_filter.py
+++ functions/yellow_white_filter.py
@@ -1,10 +1,11 @@
 import numpy as np
 import cv2 as cv
 
-def white_yellow_filter(frame):
-        hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+def yellow_white_filter(frame):
+        bgr = cv.cvtColor(frame, cv.COLOR_GRAY2BGR)
+        hsv = cv.cvtColor(bgr, cv.COLOR_BGR2HSV)
         lower_white = np.array([0, 0, 0], np.uint8)
-        upper_yellow = np.array([255, 100, 240], np.uint8)
+        upper_yellow = np.array([255, 100, 180], np.uint8)
         
         yellow_white_frame = cv.inRange(hsv, lower_white, upper_yellow)
         return yellow_white_frame
