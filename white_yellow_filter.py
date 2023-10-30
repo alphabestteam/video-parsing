a8b1d0bae30e3031182dab28ab124485eb9a457d
yoda-gell <hvuskv6@gmail.com>
2023-10-30 14:13:56 +0200

    white yellow filter

--- white_yellow_filter.py
+++ white_yellow_filter.py
@@ -3,6 +3,11 @@ import cv2 as cv
 
 def white_yellow_filter(video):
     cap = video
+    
+    fourcc = cv.VideoWriter_fourcc(*'XVID')
+    out = cv.VideoWriter('filter_white_yellow.avi', fourcc, 120, (640, 480))  # Adjust resolution as needed
+
+
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
@@ -10,13 +15,14 @@ def white_yellow_filter(video):
             break
         hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
         lower_white = np.array([0, 0, 0], np.uint8)
-        upper_yellow = np.array([255, 70, 240], np.uint8)
+        upper_yellow = np.array([255, 100, 240], np.uint8)
         
         mask = cv.inRange(hsv, lower_white, upper_yellow)
         
         cv.imshow('mask', mask) 
         if cv.waitKey(4) == ord('q'):
             break
+        
     cap.release()
     cv.destroyAllWindows()
     
@@ -25,3 +31,5 @@ def white_yellow_filter(video):
 if __name__ == "__main__":
     new_video = white_yellow_filter(cv.VideoCapture('car_driving_on_road.mp4'))
     
+    
+
