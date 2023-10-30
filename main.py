d4b87ded66b1c449b1ff27c108a386847e21a2c3
yoda-gell <hvuskv6@gmail.com>
2023-10-30 16:36:29 +0200

    .

--- main.py
+++ main.py
@@ -4,13 +4,15 @@ from functions.gaussian_blurring import gaussian_blurring
 from functions.canny import canny
 from functions.hough import hough
 from functions.size_and_slope_filter import size_and_slope_filter
+
 def main():
+    
     ro = roi()
     yellow_white = yellow_white_filter(ro)
     gau = gaussian_blurring(yellow_white)
-    ca = canny(gau)
-    hou = hough(ca)
-    size_slope = size_and_slope_filter(hou)
+    # ca = canny(gau)
+    # hou = hough(ca)
+    # size_slope = size_and_slope_filter(hou)
     
     
 
