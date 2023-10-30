acae59547ed8bcba2cb70fba520c6988893c68a9
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-10-30 14:17:58 +0200

    creating working canny algo for a video and returning it as a variable

--- canny_algo.py
+++ canny_algo.py
@@ -1,9 +1,25 @@
 import cv2
-test_img = cv2.imread("test_img.jpeg")
-low_threshold = 50
-high_threshold = 70
-canny_edge_filter = cv2.Canny(test_img, low_threshold, high_threshold)
-cv2.imshow('original', test_img)
-cv2.imshow('edge', canny_edge_filter)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+def canny_algo(car_drivinG_video):
+    frames = []
+    frame_rate = 20.0
+    video_cap = cv2.VideoCapture(car_drivinG_video)
+    fourcc = cv2.VideoWriter_fourcc(*'XVID')
+    output = cv2.VideoWriter('car_driving_on_road_canny.mp4', fourcc, 20.0, (640, 460))
+    while True:
+        video_playing, frame = video_cap.read()
+        if not video_playing:
+            break
+        low_threshold = 50
+        high_threshold = 150
+        canny_edge_filter = cv2.Canny(frame, low_threshold, high_threshold)
+        output.write(frame)
+        cv2.imshow('original', frame)
+        cv2.imshow('edge', canny_edge_filter)
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+    video_cap.release()
+    return frames, frame_rate
+
+
+if __name__ == "__main__":
+    canny_algo('car_driving_on_road.mp4')
