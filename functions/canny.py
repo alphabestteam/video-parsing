2b610410808248f1bfaaf6449517035509325c7f
Maya Meltzer <mayameltzer@Mayas-MacBook-Pro.local>
2023-11-30 18:16:28 +0200

    finishing canny function

--- functions/canny.py
+++ functions/canny.py
@@ -1,17 +1,10 @@
 import cv2
-def canny(car_drivinG_video):
-    output = cv2.VideoWriter('car_driving_on_road_canny.mp4', fourcc, 20.0, (640, 460))
-    while True:
-        video_playing, frame = video_cap.read()
-        if not video_playing:
-            break
-        low_threshold = 50
-        high_threshold = 150
-        canny_edge_filter = cv2.Canny(frame, low_threshold, high_threshold)
-        output.write(frame)
-        cv2.imshow('original', frame)
-        cv2.imshow('edge', canny_edge_filter)
-        if cv2.waitKey(1) and 0xFF == ord('q'):
-            break
-    video_cap.release()
+def canny(car_driving_frame):
+    output_image = cv2.imread(car_driving_frame)
+    output_gray = cv2.cvtColor(output_image, cv2.COLOR_BGR2GRAY)
+    low_threshold = 50
+    high_threshold = 150
+    canny_edge_filter = cv2.Canny(output_gray, low_threshold, high_threshold)
+    return canny_edge_filter
+
 
